e45971eaca36c83cd94554091da7481c90df6991
Gabrielbpinheiro14 <108081960+Gabrielbpinheiro14@users.noreply.github.com>
2022-10-08 22:11:16 -0300

    Update Ex6lista1 (2).py

--- Ex6lista1 (2).py
+++ Ex6lista1 (2).py
@@ -1,3 +1,6 @@
+#defini uma variavel para o tamanho e uma para a velocidade, printei o resultado do tempo.
+
+
 tamanho = float(input('Tamanho do arquivo (MB): '))
 velocidade = float(input('Velocidade de Internet (MBps): '))
 print('Tempo aproximado de download: %.2f Minutos ' %((tamanho / velocidade) / 60))
